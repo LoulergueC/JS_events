b610029f07b8bd3891d657ecc23b0f0966998d1b
LoulergueC <clement.loulergue@live.fr>
2023-11-20 10:56:47 +0100

    buger menu collapse

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ document.getElementsByTagName("footer")[0].addEventListener("click", () => {
     console.log("clique n°" + nclique);
     nclique++;
 });
+
+// burger menu collapse
+document
+    .getElementsByClassName("navbar-toggler")[0]
+    .addEventListener("click", () => {
+        document.getElementById("navbarHeader").classList.toggle("collapse");
+    });
